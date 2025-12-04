74c37a0d1fa1d7122c30365c2cc1196d492b67e9
MimDel <mariadelikoceva@gmail.com>
2025-12-04 16:17:25 +0200

    masonry done

--- Beauty_site/src/components/ImageMasonry.jsx
+++ Beauty_site/src/components/ImageMasonry.jsx
@@ -4,111 +4,68 @@ import Paper from '@mui/material/Paper';
 import Masonry from '@mui/lab/Masonry';
 import { styled } from '@mui/material/styles';
 
-const Label = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(0.5),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-    ...theme.applyStyles('dark', {
-        backgroundColor: '#1A2027',
-    }),
-}));
-
 export default function ImageMasonry() {
     return (
-        <Box sx={{ width: 500, minHeight: 829 }}>
-            <Masonry columns={3} spacing={2}>
-                {itemData.map((item, index) => (
-                    <div key={index}>
-                        <img
-                            srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-                            src={`${item.img}?w=162&auto=format`}
-                            alt={item.title}
-                            loading="lazy"
-                            style={{
-                                borderBottomLeftRadius: 4,
-                                borderBottomRightRadius: 4,
-                                display: 'block',
-                                width: '100%',
-                            }}
-                        />
-                    </div>
-                ))}
-            </Masonry>
-        </Box>
+        <div className='flex flex-row justify-center'>
+            <Box sx={{ width: '80%', minHeight: 829 }}>
+                <Masonry columns={3} spacing={2}>
+                    {itemData.map((item, index) => (
+                        <div key={index}>
+                            <img
+                                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+                                src={`${item.img}?w=162&auto=format`}
+                                alt={item.title}
+                                loading="lazy"
+                                style={{
+                                    borderBottomLeftRadius: 4,
+                                    borderBottomRightRadius: 4,
+                                    display: 'block',
+                                    width: '100%',
+                                }}
+                            />
+                        </div>
+                    ))}
+                </Masonry>
+            </Box>
+        </div>
     );
 }
 
 const itemData = [
     {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
+        img: 'http://localhost:3000/trEssenceMist/tr_essenceMist1.png',
+        title: 'tr_essenceMist1',
     },
     {
-        img: 'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
-        title: 'Snacks',
+        img: 'http://localhost:3000/trMoistureCream/tr_moistureCream3.jpg',
+        title: 'tr_moistureCream3',
     },
     {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
+        img: 'http://localhost:3000/saBalmCream/sa_balmCream3.webp',
+        title: 'sa_balmCream3',
     },
     {
-        img: 'https://images.unsplash.com/photo-1529655683826-aba9b3e77383',
-        title: 'Tower',
+        img: 'http://localhost:3000/saAmpoule/sa_ampoule2.webp',
+        title: 'sa_ampoule2',
     },
     {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
+        img: 'http://localhost:3000/saCream/sa_cream2.webp',
+        title: 'sa_cream2',
     },
     {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
+        img: 'http://localhost:3000/trCleansingWater/tr_cleansingWater3.webp',
+        title: 'tr_cleansingWater3',
     },
     {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
+        img: 'http://localhost:3000/trAmpoule/tr_ampoule2.webp',
+        title: 'tr_ampoule2',
     },
     {
-        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-        title: 'Breakfast',
+        img: 'http://localhost:3000/trEssence/tr_essence3.webp',
+        title: 'tr_essence3',
     },
     {
-        img: 'https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d',
-        title: 'Tree',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1627000086207-76eabf23aa2e',
-        title: 'Camping Car',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-        title: 'Hats',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7',
-        title: 'Mountain',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
+        img: 'http://localhost:3000/saSerum/sa_serum2.webp',
+        title: 'sa_serum2',
     },
 ];
